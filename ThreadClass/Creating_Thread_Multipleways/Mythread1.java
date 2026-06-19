badb075e0b9569f2fb7345616e908e911c168d6d
ashishmohanty02 <ashishmohanty671@gmail.com>
2026-06-19 15:27:25 +0530

    How LambdaExpression(Thread class and Runnable interface) are converted into custom classes by JVM internally

--- ThreadClass/Creating_Thread_Multipleways/Mythread1.java
+++ ThreadClass/Creating_Thread_Multipleways/Mythread1.java
@@ -1,10 +1,11 @@
 package Threads.ThreadClass.Creating_Thread_Multipleways;
 
 class ThreadTask1 extends Thread{ // going to created in metaspace
+    int counter=0;
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+" started ....");
-        System.out.println(Thread.currentThread().getName()+" terminated ....");
+        counter++;
+        System.out.println(Thread.currentThread().getName()+" "+counter);
     }
 }
 
@@ -15,7 +16,11 @@ public class Mythread1 { // meta space
         ThreadTask1 t2=new ThreadTask1(); //new ThreadTask2() in common shared heap(e.g 8k memory address) and t2 in main stack pointing to 8k
         t1.start(); // allocation of new private stack and loading of start method as activation recored with address 12k
         t2.start(); // allocation of new private stack and loading of start method as activation recored with address 98k
-
+        ThreadTask1 t3=new ThreadTask1();
+        Thread t4=new Thread(t3);
+        Thread t5=new Thread(t3);
+        t4.start();
+        t5.start();
         System.out.println("Main thread completed its execution");
     }
 }
